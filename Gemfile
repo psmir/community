d55c93a29eab9a6ca3ab2ef93ea26a56133ead15
Peter <psmir777@gmail.com>
2018-06-24 09:59:36 +0300

    Configure Devise + Hyeprloop

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 ####################################
 gem 'hyperloop', '~> 0.5.8'
-gem 'devise'
+gem 'devise', '~> 4.4.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/hyperloop/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable unless RUBY_ENGINE == 'opal'
+
+  def self.current
+    Hyperloop::Application.acting_user_id ? find(Hyperloop::Application.acting_user_id) : User.new
+  end
+
+end
