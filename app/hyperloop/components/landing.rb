e8b9c7c99385b9e726e7c4b43bdd6b77df5ea24b
Peter <psmir777@gmail.com>
2018-08-12 22:33:07 +0300

    Display posts on the landing page

--- app/hyperloop/components/landing.rb
+++ app/hyperloop/components/landing.rb
@@ -2,9 +2,13 @@ class Landing < Hyperloop::Router::Component
 
   render(SECTION) do
     DIV(class: 'container') do
-      DIV(class: 'row viewport-80 align-items-center') do
-        DIV(class: 'col col-md-6 mx-auto') do
-          H1 { 'Welcome to Community' }
+      DIV(class: 'row viewport-80') do
+        DIV(class: 'col col-md-10 mx-auto') do
+          H1(class: 'm-3 text-center') { 'Welcome to Community' }
+
+          Post.recent.each do |p|
+            PostItem(post: p)
+          end
         end
       end
     end
---
+++ app/hyperloop/components/post_item.rb
@@ -0,0 +1,34 @@
+class PostItem < Hyperloop::Component
+  include ViewHelpers
+
+  param :post
+
+  state expanded: false
+
+  render(DIV, class: 'card m-5') do
+    DIV(class: 'card-header') do
+      H2(class: 'card-title'){ params.post.title }
+      "Posted By #{params.post.user.username} at #{params.post.created_at(:short)}"
+    end
+
+    DIV(class: 'card-body') do
+      P do
+        if state.expanded
+          params.post.body
+        else
+          truncate(params.post.body, length: 300)
+        end
+      end
+    end
+
+    DIV(class: 'card-footer text-right', style: { 'min-height' => '4rem'}) do
+      if params.post.body.length > 300
+        A(class: 'btn btn-primary') { state.expanded ? 'Narrow' : 'Expand' }
+        .on(:click) do |evt|
+          evt.prevent_default
+          mutate.expanded !state.expanded
+        end
+      end
+    end
+  end
+end
